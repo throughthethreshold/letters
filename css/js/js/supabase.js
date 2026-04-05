39282e09f4a2fb14d1397fe753649cf57347f06c
throughthethreshold <thefirstdraftsclub@gmail.com>
2026-04-05 17:33:35 +0200

    Delete css directory

--- css/js/js/supabase.js
+++
@@ -1,5 +0,0 @@
-const SUPABASE_URL = 'https://lyoxsgcuavigbenldqlw.supabase.co'
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx5b3hzZ2N1YXZpZ2JlbmxkcWx3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUzODQ1MTIsImV4cCI6MjA5MDk2MDUxMn0.CFyrmjkXGO8XjZZ6BUAAoA-yj4WDjKfCqIXgQXgmZGk'
-
-const { createClient } = supabase
-const db = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
